d4a07c4ca20c160a377e6afc22b990674923bc30
Artem Dunaev <dunaev.aa@yandex.ru>
2020-01-20 12:06:56 +0500

    add arguments and format option

--- src/gendiff/index.js
+++ src/gendiff/index.js
@@ -4,6 +4,11 @@ const gendiff = () => {
   program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      console.log({ firstConfig, secondConfig });
+    })
     .parse(process.argv);
 };
 
