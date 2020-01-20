fe6501da8f8e0a0011cab163d10491bcba4257dc
Artem Dunaev <dunaev.aa@yandex.ru>
2020-01-20 16:00:31 +0500

    compare plane files

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,30 +1,33 @@
-import genDiff from "../src/gendiff";
+import genDiff, { linesToString } from "../src/gendiff";
 
 test('compare plane files', () => {
   const before = {
-    "host": "hexlet.io",
-    "timeout": 50,
-    "proxy": "123.234.53.22",
-    "follow": false
+    host: "hexlet.io",
+    timeout: 50,
+    proxy: "123.234.53.22",
+    follow: false
   };
 
   const after = {
-    "timeout": 20,
-    "verbose": true,
-    "host": "hexlet.io"
+    timeout: 20,
+    verbose: true,
+    host: "hexlet.io"
   };
 
   // const beforeJSON = JSON.stringify(before);
   // const afterJSON = JSON.stringify(after);
 
   const result = `{
-      host: hexlet.io
-    + timeout: 20
-    - timeout: 50
-    - proxy: 123.234.53.22
-    + verbose: true
-    - follow: false
-  }`;
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  - follow: false
+  + verbose: true
+}`;
 
-  expect(genDiff(before, after)).toEqual(result);
+  const diff = genDiff(before, after);
+  const diffStr = linesToString(diff);
+  console.log(diffStr);
+  expect(diffStr).toEqual(result);
 });
--- src/gendiff/index.js
+++ src/gendiff/index.js
@@ -1,3 +1,5 @@
+import { has } from 'lodash';
+
 // import program from 'commander';
 //
 // const main = () => {
@@ -22,11 +24,62 @@
 // const data1 = getData(file1);
 // const data2 = getData(file2);
 //
-// use: lodash has
+// use: lodash has +
 // const diff = genDiff(data1, data2);
 
+// как представить строку? в виде массива из трех значений?
+// знак: + (добавлен), - (удален), пусто (без изменений)
+// [ 'знак', 'ключ', 'значение' ]
+
 const genDiff = (data1, data2) => {
-  return ''
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+
+  const result = keys1
+    .reduce((acc, key) => {
+      const value1 = data1[key];
+      const value2 = data2[key];
+
+      if (!keys2.includes(key)) {
+        return [...acc, ['-', key, value1]]
+      }
+
+      if (value1 === value2) {
+        return [...acc, [' ', key, value1]]
+      } else {
+        return [...acc,
+          ['+', key, value2],
+          ['-', key, value1]
+        ]
+      }
+    }, []);
+
+  const addedKeys = keys2
+    .reduce((acc ,key) => (!keys1.includes(key))
+      ? [...acc, ['+', key, data2[key]]]
+      : acc, []);
+
+  return [...result, ...addedKeys]
+};
+
+export const linesToString = (lines) => {
+  const lineBreak = "\n";
+  const offset = " ".repeat(2);
+  const result = [];
+  for (const line of lines) {
+    const [sign, key, value] = line;
+    const string = `${sign} ${key}: ${value}`;
+    result.push(`${offset}${string}${lineBreak}`);
+  }
+  return `{\n${result.join('')}}`;
 };
 
 export default genDiff;
+
+// может на множествах сделать?
+// const set1 = new Set(keys1);
+// const set2 = new Set(keys2);
+//
+// const changed = new Set([...set1].filter(x => set2.has(x)))
+// const deleted = new Set([...set1].filter(x => !set2.has(x)));
+// const added = new Set([...set2].filter(x => !set1.has(x)));
