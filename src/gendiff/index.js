c55dc4442f1cacc89cc8872792a4986f08834634
Artem Dunaev <dunaev.aa@yandex.ru>
2020-01-20 11:36:35 +0500

    add auto generated help

--- src/gendiff/index.js
+++ src/gendiff/index.js
@@ -1,28 +1,10 @@
 import program from 'commander';
 
 const gendiff = () => {
-  program.version('0.0.1');
-
   program
-    .option('-V, --version', 'output the version number')
-    .option('-h, --help', 'output usage information');
-
-  program.parse(process.argv);
-
-  console.log('is works!');
-
-  // description
-  // version
-
-  // gendiff -h
-  //
-  // Usage: gendiff [options]
-  //
-  // Compares two configuration files and shows a difference.
-  //
-  //   Options:
-  // -V, --version        output the version number
-  // -h, --help           output usage information
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.')
+    .parse(process.argv);
 };
 
 export default gendiff;
