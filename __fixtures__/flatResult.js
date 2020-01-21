cde40c30b6ef4de2bc9f57baf64db800d0fb36ac
Artem Dunaev <dunaev.aa@yandex.ru>
2020-01-21 11:46:25 +0500

    fix tests

---
+++ __fixtures__/flatResult.js
@@ -0,0 +1,10 @@
+const result = `{
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  - follow: false
+  + verbose: true
+}`;
+
+export default result;
--- __tests__/genDiff.js
+++ __tests__/genDiff.js
@@ -1,19 +1,11 @@
 import path from 'path';
 import genDiff from '../src';
+import result from '../__fixtures__/flatResult';
 
 test('compare flat json files', () => {
   const fixturesPath = '__fixtures__';
   const path1 = path.join(fixturesPath, 'before.json');
   const path2 = path.join(fixturesPath, 'after.json');
-
-  const result = `{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  - follow: false
-  + verbose: true
-}`;
 
   expect(genDiff(path1, path2)).toEqual(result);
 });
