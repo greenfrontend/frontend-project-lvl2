233c67b64703b53a07f230d07eace66eadf42305
Artem Dunaev <dunaev.aa@yandex.ru>
2020-01-22 12:55:07 +0500

    add nested tests

--- __tests__/genDiff.js
+++ __tests__/genDiff.js
@@ -1,16 +1,24 @@
 import path from 'path';
 import genDiff from '../src';
-import result from '../__fixtures__/flatResult';
+import resultFlat from '../__fixtures__/resultFlat';
+import resultNested from '../__fixtures__/resultNested';
 
 const formats = ['json', 'yml', 'ini'];
 const fixturesPath = '__fixtures__';
 
-const testTable = formats.map((format) => [
-  format,
-  path.join(fixturesPath, `before.${format}`),
-  path.join(fixturesPath, `after.${format}`),
-]);
+// const testTable = formats.map((format) => [
+//   format,
+//   path.join(fixturesPath, `before.${format}`),
+//   path.join(fixturesPath, `after.${format}`),
+// ]);
+//
+// test.each(testTable)('compare files %s', (format, path1, path2) => {
+//   expect(genDiff(path1, path2)).toEqual(resultFlat);
+// });
 
-test.each(testTable)('compare files %s', (format, path1, path2) => {
-  expect(genDiff(path1, path2)).toBe(result);
+test('compare nested files', () => {
+  const path1 = path.join(fixturesPath, `beforeNested.json`);
+  const path2 = path.join(fixturesPath, `afterNested.json`);
+
+  expect(genDiff(path1, path2)).toEqual(resultNested)
 });
