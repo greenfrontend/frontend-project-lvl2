dd23a7e9ad1a50cb05106c1514a81f47812238ce
Artem Dunaev <dunaev.aa@yandex.ru>
2020-02-25 00:23:18 +0530

    change reduce to map at recursive formatter

--- src/formatters/recursive.js
+++ src/formatters/recursive.js
@@ -26,10 +26,10 @@ const format = (ast, level = 0) => {
   const baseOffset = '  ';
   const offset = baseOffset + baseOffset.repeat(level * 2);
 
-  return ast.reduce((acc, node) => {
+  return ast.map((node) => {
     switch (node.type) {
       case types.changed:
-        return flatten([...acc, format([{
+        return flatten(format([{
           type: 'deleted',
           key: node.key,
           value: node.oldValue,
@@ -37,27 +37,27 @@ const format = (ast, level = 0) => {
           type: 'added',
           key: node.key,
           value: node.newValue,
-        }], level)]);
+        }], level));
       case types.nested:
-        return [...acc,
+        return flatten([
           `${offset}${signs[node.type]} ${node.key}: {`,
           ...format(node.children, level + 1),
           `${offset}  }`,
-        ];
+        ]);
       case types.added:
       case types.deleted:
       case types.unchanged:
-        return [...acc, `${offset}${signs[node.type]} ${node.key}: ${getValue(node, level)}`];
+        return [`${offset}${signs[node.type]} ${node.key}: ${getValue(node, level)}`];
       default:
         return null;
     }
-  }, []);
+  });
 };
 
 const formatToString = (lines) => `{\n${lines.join('\n')}\n}`;
 
 export default (ast) => {
-  const lines = format(ast);
+  const lines = flatten(format(ast));
   const result = formatToString(lines);
   return result;
 };
